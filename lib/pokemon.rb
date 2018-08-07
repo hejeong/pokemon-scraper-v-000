6d6cb2bc5b71ad136b051bf1d70861b9e594c047
Jonathan Hong <jonhong777@gmail.com>
2018-08-07 18:27:33 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,4 +16,11 @@ class Pokemon
     db.execute(sql, name, type)
     @id = db.execute("SELECT last_insert_rowid() FROM pokemon")
   end
+
+  def self.find(id, db)
+    sql = <<-SQL
+          SELECT * FROM pokemon WHERE id = ?
+          SQL
+    db.execute(sql, id)
+  end
 end
